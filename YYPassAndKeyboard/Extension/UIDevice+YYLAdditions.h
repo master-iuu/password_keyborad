0dc8edf8707c0d422189bd3cbefeaaf671cb4e7f
Chic-Yang <henlafen@163.com>
2018-08-14 10:28:00 +0800

    添加分类文件

---
+++ YYPassAndKeyboard/Extension/UIDevice+YYLAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIDevice+YYLAdditions.h
+//  YYPassAndKeyboard
+//
+//  Created by 杨永亮 on 2018/8/14.
+//  Copyright © 2018年 cstroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (YYLAdditions)
+
+@end
---
+++ YYPassAndKeyboard/Extension/UIDevice+YYLAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+YYLAdditions.m
+//  YYPassAndKeyboard
+//
+//  Created by 杨永亮 on 2018/8/14.
+//  Copyright © 2018年 cstroll. All rights reserved.
+//
+
+#import "UIDevice+YYLAdditions.h"
+
+@implementation UIDevice (YYLAdditions)
+
+@end
